b59d052e5548fde27888cd7a532a17464a254537
sreenithi sathiyamoorthi <146321251+sreesathiya175@users.noreply.github.com>
2024-07-05 22:04:51 +0530

    Add files via upload

---
+++ bbb.js
@@ -0,0 +1,94 @@
+const quizData = [
+    {
+        question: "What is the capital of France?",
+        a: "New York",
+        b: "London",
+        c: "Paris",
+        d: "Berlin",
+        correct: "c",
+    },
+    {
+        question: "Who is the CEO of Tesla?",
+        a: "Jeff Bezos",
+        b: "Elon Musk",
+        c: "Bill Gates",
+        d: "Tony Stark",
+        correct: "b",
+    },
+    {
+        question: "What is the largest ocean?",
+        a: "Atlantic Ocean",
+        b: "Indian Ocean",
+        c: "Arctic Ocean",
+        d: "Pacific Ocean",
+        correct: "d",
+    },
+    {
+        question: "Which language is used for web development?",
+        a: "Python",
+        b: "C++",
+        c: "JavaScript",
+        d: "Java",
+        correct: "c",
+    }
+];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false);
+}
+
+function getSelected() {
+    let answer;
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
